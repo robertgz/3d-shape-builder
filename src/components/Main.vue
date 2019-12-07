0254daced53215bba953edc8a1d206ad64ec691d
Robert <robertgz@users.noreply.github.com>
2019-12-06 19:01:02 -0800

    added Main component

---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: 'Main Vue app'
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'https://unpkg.com/vue@2.6.10/dist/vue.esm.browser.js';
 
+import Main from './components/Main.vue';
+
 function mainVue() {
 
   var app = new Vue({
     el: '#app',
-    template: '<div>Vue app</div>'
+    components: {
+      Main,
+    },
+    template: '<div><Main></Main></div>',
   });
 
 }
