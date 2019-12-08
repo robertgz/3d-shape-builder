98ac9b07d9efcc28f8c6e9bca05a9b487b0886d1
Robert <robertgz@users.noreply.github.com>
2019-12-07 23:33:35 -0800

    moved OrbitControls into a component

---
+++ src/components/OrbitControlsComponent.vue
@@ -0,0 +1,41 @@
+<template>
+</template>
+
+<script>
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+export default {
+  props: { 
+    object: {
+      type: Object,
+      required: true
+    },
+    renderer: Object,
+    properties: Object,
+  },
+  computed: {
+  },
+
+  data () {
+    return {
+      orbitControl: null,
+    }
+  },
+
+  methods: {
+    updateProperties: function() {
+      for (const property in this.properties) {
+        this.orbitControl[property] = this.properties[property];
+      }
+    }
+  },
+  
+  mounted: function () {
+    this.orbitControl = new OrbitControls( this.object, this.renderer.domElement );
+    this.updateProperties();
+  },
+}
+</script>
+
+<style scoped>
+</style>
--- src/components/SceneRoot.vue
+++ src/components/SceneRoot.vue
@@ -1,19 +1,31 @@
 <template>
   <div v-bind:id="canvasID">
-    <StatsComponent v-bind:parentNodeSelector="canvasNodeSelector" v-bind:panel="0"></StatsComponent>
+    <StatsComponent 
+      v-bind:parentNodeSelector="canvasNodeSelector" 
+      v-bind:panel="0"
+    ></StatsComponent>
+    <OrbitControlsComponent 
+      v-bind:object="camera" 
+      v-bind:renderer="renderer" 
+      v-bind:properties="{zoomSpeed: 1}"
+    ></OrbitControlsComponent>
   </div>
 </template>
 
 <script>
 import * as THREE from 'three';
 import StatsComponent from './StatsComponent.vue';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import OrbitControlsComponent from './OrbitControlsComponent.vue';
 
 export default {
   data () {
     return {
       canvasID: "sceneRoot",
       sceneRootNode: null,
+      renderer: null,
+      scene: null,
+      camera: null,
+      cube: null,
     }
   },
 
@@ -25,44 +37,45 @@ export default {
 
   components: {
     StatsComponent,
+    OrbitControlsComponent,
   },
 
   methods: {
     // Based on code from: https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene
     init: function() {
       this.sceneRootNode = document.querySelector( this.canvasNodeSelector );
-
-      var scene = new THREE.Scene();
-      var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
-      var renderer = new THREE.WebGLRenderer();
-      renderer.setSize( window.innerWidth, window.innerHeight );
-      this.sceneRootNode.appendChild( renderer.domElement );
-
+      this.sceneRootNode.appendChild( this.renderer.domElement );
+    },
+    createObject: function() {
       var geometry = new THREE.BoxGeometry( 1, 1, 1 );
       var material = new THREE.MeshBasicMaterial( { color: 0x44aa88 } );
-      var cube = new THREE.Mesh( geometry, material );
-      scene.add( cube );
+      this.cube = new THREE.Mesh( geometry, material );
+      this.scene.add( this.cube );
+    },
+    animate: function() {
+      requestAnimationFrame( this.animate );
 
-      camera.position.z = 5;
-      
-      var orbitControl = new OrbitControls( camera, renderer.domElement );
+      this.cube.rotation.x += 0.01;
+      this.cube.rotation.y += 0.01;
 
-      function animate() {
-        requestAnimationFrame( animate );
-
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
-        
-        renderer.render( scene, camera );
-      }
-      animate();
-
-    }
+      this.renderer.render( this.scene, this.camera );
+    },
   },
-  
+
+  created: function() {
+    this.renderer = new THREE.WebGLRenderer();
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );  
+    
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    this.camera.position.z = 5;
+
+    this.createObject(); 
+  },
+
   mounted: function () {
-    this.init();
+    this.init();       
+    this.animate();
   }
 }
 </script>
