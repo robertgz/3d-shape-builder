bdef48808cca83dab843cdad087f55cd91d1cbdc
Robert <robertgz@users.noreply.github.com>
2019-12-28 14:26:18 -0800

    added select.store

--- src/store/index.store.js
+++ src/store/index.store.js
@@ -2,13 +2,17 @@ import Vue from 'vue'
 import Vuex from 'vuex';
 import meshes from './meshes.store';
 import scene from './scene.store';
+import mouse from './mouse.store';
+import select from './select.store';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    md: meshes,
-    sc: scene,
+    meshes: meshes,
+    scene: scene,
+    mouse: mouse,
+    select: select,
   },
   state: {},
   getters: {},
---
+++ src/store/select.store.js
@@ -0,0 +1,77 @@
+import * as THREE from 'three';
+
+const selection = {
+  namespaced: true,
+  modules: {},
+
+  state: {
+    raycaster: new THREE.Raycaster(),
+    selectedSet: new Set(),
+    selectionActive: false,
+    mouseUnwatch: null,
+  },
+
+  getters: {
+    getSelectionStatus(state) {
+      return state.selectionActive;
+    },
+  },
+
+  actions: {
+    
+    doRayCasting(context, payload) {
+      let camera = context.rootGetters['getCamera'];
+      let scene = context.rootGetters['getScene'];
+      let mouse = context.rootGetters['mouse/locationVector'];
+
+      context.state.raycaster.setFromCamera( mouse, camera );
+      
+      let intersects = context.state.raycaster.intersectObjects( scene.children );
+
+      for ( var i = 0; i < intersects.length; i++ ) {
+        intersects[ i ].object.material.color.set( 0x00ff00 );
+        // console.log('intersected object.id: ', intersects[ i ].object.id);
+      }
+
+      if (intersects.length > 0) {
+        context.state.selectedSet.add(intersects[ 0 ].object.id);
+      } else if (intersects.length === 0) {
+        context.state.selectedSet.clear();
+      }
+      console.log('context.state.selectedSet', context.state.selectedSet);
+    },
+
+    enableSelect(context) {
+      // console.log('enableSelect');
+
+      context.state.selectionActive = true;
+      context.dispatch('mouse/enableListening', null, { root: true });
+
+      context.state.mouseUnwatch = this.watch(function () {
+        return context.rootGetters['mouse/location'];
+      }, 
+      function (newVal, oldVal) {
+        context.dispatch('doRayCasting');
+      },
+        { deep: true }
+      );
+    },
+
+    disableSelect(context) {
+      // console.log('disableSelect');
+
+      context.state.selectionActive = false;
+      context.dispatch('mouse/disableListening', null, { root: true });
+
+      if ( context.state.mouseUnwatch ) {
+        context.state.mouseUnwatch(); 
+      }
+    },
+
+  },
+
+  mutations: {
+  },
+};
+
+export default selection;
