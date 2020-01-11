4e9ab5e2ef0681bf98a94bfa9096512853e6dcc5
Robert <robertgz@users.noreply.github.com>
2020-01-11 15:25:24 -0800

    changed build system

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,25 @@
 import Vue from 'vue'
-import Vuex from 'vuex';
+import Vuex from 'vuex'
 import objects from './objects.store';
 import scene from './scene.store';
 import mouse from './mouse.store';
 import select from './select.store';
 import controls from './controls.store';
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
   modules: {
     objects: objects,
     scene: scene,
     mouse: mouse,
     select: select,
     controls: controls,
-  },
-  state: {},
-  getters: {},
-  actions: {},
-  mutations: {},
-});
+  }
+})
