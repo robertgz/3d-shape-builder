1dcef2dac2ad3c8583a67bcdfeff22e56db9e2bd
Robert <robertgz@users.noreply.github.com>
2020-02-09 19:14:58 -0800

    HelpersComponent: document warning with GridHelper

--- src/components/HelpersComponent.js
+++ src/components/HelpersComponent.js
@@ -28,6 +28,9 @@ export default {
       this.gridHelper = new THREE.GridHelper( this.size, this.divisions );
       this.gridHelper.rotateX(Math.PI / 2.0);
       this.helpersNode.add( this.gridHelper );
+      // In Three 113.2 GridHelper shows two errors with Chrome Version 82.0.4051.0 (Official Build) canary (32-bit) are seen,
+      //    but not in Firefox, nor Chrome 80
+      // EXT_frag_depth, EXT_shader_texture_lod extensions not supported.
     },
 
   },
