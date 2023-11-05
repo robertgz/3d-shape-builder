8be78da440025ec46043f528dd085958aa100ba3
Robert <robertgz@users.noreply.github.com>
2023-11-05 11:59:08 -0800

    Migrated vite-plugin-vue2 to @vitejs/plugin-vue2

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 
-import { defineConfig } from 'vite'
-import { createVuePlugin as vue } from "vite-plugin-vue2";
+import vue from '@vitejs/plugin-vue2'
 import { VuetifyResolver } from "unplugin-vue-components/resolvers";
 import Components from "unplugin-vue-components/vite";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [
     vue(),
     Components({
@@ -22,4 +21,4 @@ export default defineConfig({
       "@": path.resolve(path.dirname(''), "./src"),
     },
   },
-})
+}
